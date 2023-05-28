f3d3d90c3b5c2d25674fc304335b35a1ea0f824f
skorpion <jmosinski@o2.pl>
2023-05-28 19:46:27 +0200

    next if else js for section contact

--- js/contact.js
+++ js/contact.js
@@ -130,6 +130,67 @@ const send = () => {
 			inputMail.value = ''
 			textarea.value = ''
 		}, 3000)
+	} else if (inputName.value !== '' && inputMail.value === '' && textarea.value === '') {
+		sendStatus.classList.add('senderror')
+		textStatus.textContent = 'Bład wysłania wiadomości'
+		errorName.classList.remove('actionerror')
+		errorMail.classList.add('actionerror')
+		errorMail.textContent = 'Wypełnij pole'
+		errorText.classList.add('actionerror')
+		errorText.textContent = 'Wypełnij pole'
+		inputMail.classList.add('backgrounderror')
+		textarea.classList.add('backgrounderror')
+		setTimeout(() => {
+			sendStatus.classList.remove('senderror')
+			errorMail.classList.remove('actionerror')
+			errorText.classList.remove('actionerror')
+			inputMail.classList.remove('backgrounderror')
+			textarea.classList.remove('backgrounderror')
+			inputName.value = ''
+			inputMail.value = ''
+			textarea.value = ''
+		}, 3000)
+	} else if (inputName.value === '' && inputMail.value !== '' && textarea.value === '') {
+		sendStatus.classList.add('senderror')
+		textStatus.textContent = 'Bład wysłania wiadomości'
+		errorName.classList.add('actionerror')
+		errorName.textContent = 'Wypełnij pole'
+		errorMail.classList.remove('actionerror')
+		errorText.classList.add('actionerror')
+		errorText.textContent = 'Wypełnij pole'
+		inputName.classList.add('backgrounderror')
+		textarea.classList.add('backgrounderror')
+		setTimeout(() => {
+			sendStatus.classList.remove('senderror')
+			errorName.classList.remove('actionerror')
+			errorText.classList.remove('actionerror')
+			inputName.classList.remove('backgrounderror')
+			textarea.classList.remove('backgrounderror')
+			inputName.value = ''
+			inputMail.value = ''
+			textarea.value = ''
+		}, 3000)
+	} else if (inputName.value === '' && inputMail.value === '' && textarea.value !== '') {
+		sendStatus.classList.add('senderror')
+		textStatus.textContent = 'Bład wysłania wiadomości'
+		errorName.classList.add('actionerror')
+		errorName.textContent = 'Wypełnij pole'
+		errorMail.classList.add('actionerror')
+		errorMail.textContent = 'Wypełnij pole'
+		errorText.classList.remove('actionerror')
+		errorText.textContent = 'Wypełnij pole'
+		inputName.classList.add('backgrounderror')
+		inputMail.classList.add('backgrounderror')
+		setTimeout(() => {
+			sendStatus.classList.remove('senderror')
+			errorName.classList.remove('actionerror')
+			errorMail.classList.remove('actionerror')
+			inputName.classList.remove('backgrounderror')
+			inputMail.classList.remove('backgrounderror')
+			inputName.value = ''
+			inputMail.value = ''
+			textarea.value = ''
+		}, 3000)
 	}
 }
 
