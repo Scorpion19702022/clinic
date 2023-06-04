cf62b57fd86c6c26048d92a3f31b6962b2fdf251
skorpion <jmosinski@o2.pl>
2023-06-04 16:39:34 +0200

    corrected js for scrollspy

--- js/scrollspy.js
+++ js/scrollspy.js
@@ -46,17 +46,17 @@ const navOpinions = document.querySelector('.opinions')
 
 const scrollSpy = () => {
 	console.log(window.scrollY)
-	if (window.scrollY >= 925 && window.scrollY < 1850) {
+	if (window.scrollY >= 625 && window.scrollY < 1550) {
 		navHome.classList.remove('scrollactive')
 		navAbout.classList.add('scrollactive')
 		navOffer.classList.remove('scrollactive')
 		navOpinions.classList.remove('scrollactive')
-	} else if (window.scrollY >= 1850 && window.scrollY < 2635) {
+	} else if (window.scrollY >= 1550 && window.scrollY < 2335) {
 		navHome.classList.remove('scrollactive')
 		navAbout.classList.remove('scrollactive')
 		navOffer.classList.add('scrollactive')
 		navOpinions.classList.remove('scrollactive')
-	} else if (window.scrollY >= 2635) {
+	} else if (window.scrollY >= 2335) {
 		navHome.classList.remove('scrollactive')
 		navAbout.classList.remove('scrollactive')
 		navOffer.classList.remove('scrollactive')
